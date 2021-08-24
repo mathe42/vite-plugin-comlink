5386a506b6f94e624487cca28c057d88d6a602fe
Sebastian Krüger <2pi_r2@gmx.de>
2021-08-24 21:13:22 +0200

    v1.0.0

--- README.md
+++ README.md
@@ -1,5 +1,101 @@
-# Vite-plugin-comlink
+# vite-plugin-comlink
 
-Plugin to enable comlink use of workers with simple import.
+Use Workers with comlink
 
-> NOTE: This Plugin is WIP so currently the build doesn't work properly.
+### Install
+
+```sh
+npm i --save-dev vite-plugin-comlink # yarn add -D vite-plugin-comlink
+```
+
+### Add it to vite.config.js
+
+```ts
+// vite.config.js
+import comlink from 'vite-plugin-comlink'
+
+export default {
+  plugins: [
+    comlink({types: true})
+  ],
+}
+```
+
+
+## Options
+
+```ts
+// vite.config.js
+import comlink from 'vite-plugin-comlink'
+
+export default {
+  plugins: [
+    comlink({
+      /**
+       * Enable type generation
+       * @default false
+       */
+      types: false,
+      /**
+       * Set import schema to use
+       * @default comlink:
+       */
+      schema: "comlink:",
+      /**
+       * Internal plugins that are used in worker build.
+       * 
+       * Use only when you know what you do!
+       * 
+       * @default "see source code"
+       */
+      internal_worker_plugins: [],
+      /**
+       * Filename of type file
+       * @default comlink.d.ts
+       */
+      typeFile: "comlink.d.ts"
+    })
+  ],
+}
+```
+
+## Plugins in Worker build
+To use a Plugin in the worker build you have to wrap it in the exportet `inWorker` function like this:
+
+```ts
+// vite.config.ts
+
+import { inWorker } from 'vite-plugin-comlink'
+import customPlugin from 'vite-plugin-custom'
+
+export default {
+  plugins: [
+    inWorker(customPlugin({
+      optionA: true
+    }))
+  ]
+}
+```
+
+This will add an internal flag so we can detect these modules.
+
+
+## Types
+See https://github.com/GoogleChromeLabs/comlink#typescript for some drawbacks / limitations of typescript.
+
+If you enable types, a `.d.ts` file is generated at runtime that types your imports! This makes typescript realy simple. Ensure that typescript includes the `.d.ts` file.
+
+## Drawbacks
+Not all Browsers support module Workers (see https://caniuse.com/mdn-api_worker_worker_ecmascript_modules). This results in some Drawbacks for fastest and best compat.
+
+As Safari TP just added support I will add a option to change to the module Workers in production which will only work in the listed Browsers.
+
+### Dev
+You have to develop in a Browser that supports module Workers: (https://caniuse.com/mdn-api_worker_worker_ecmascript_modules)
+
+### Production
+Each worker ist bundled in a new context. So no code spliting betwen Workers and / or main app. This results in larger bundles and loadtime!
+
+## Ressources
+https://github.com/GoogleChromeLabs/comlink
+https://github.com/surma/rollup-plugin-comlink
--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,202 @@
 import { readFileSync, writeFileSync, existsSync } from "fs";
+import { createHash } from "crypto";
 import { join } from "path";
-import { Plugin } from "vite";
+import { parse as parseUrl, URLSearchParams } from "url";
+import { Plugin, ResolvedConfig } from "vite";
+import { rollup } from "rollup";
 
-export default function comlink({ types = false }: { types: boolean }): Plugin {
-  const SCHEMA_MAIN_THREAD = "comlink:";
-  const SCHEMA_WORKER = "comlink-worker:";
+function getAssetHash(content: Buffer): string {
+  return createHash("sha256").update(content).digest("hex").slice(0, 8);
+}
+
+export function inWorker(plugin: Plugin): Plugin {
+  (plugin as any)._inWorker = true;
+  return plugin;
+}
+
+let internal_schema = "comlink-internal:";
+
+interface ComlinkPluginOptions {
+  /**
+   * Enable type generation
+   * @default false
+   */
+  types?: boolean;
+  /**
+   * Set import schema to use
+   * @default comlink:
+   */
+  schema?: string;
+  /**
+   * Internal plugins that are used in worker build.
+   * 
+   * Use only when you know what you do!
+   */
+  internal_worker_plugins?: string[];
+  /**
+   * Filename of type file
+   * @default comlink.d.ts
+   */
+  typeFile?: string;
+}
+
+export default function comlink({
+  types = false,
+  schema = "comlink:",
+  typeFile = "comlink.d.ts",
+  internal_worker_plugins = [
+    "alias",
+    "vite:modulepreload-polyfill",
+    "vite:resolve",
+    "vite:esbuild",
+    "vite:json",
+    "vite:wasm",
+    "vite:asset",
+    "vite:define",
+    "commonjs",
+    "vite:data-uri",
+    "rollup-plugin-dynamic-import-variables",
+    "asset-import-meta-url",
+    "vite:import-analysis",
+    "vite:esbuild-transpile",
+    "vite:terser",
+    "vite:reporter",
+    "load-fallback",
+  ],
+}: ComlinkPluginOptions = {}): Plugin {
+  let typesFile = "";
   let data: Record<string, string> = {};
-  let typeFile: string;
+
+  let isBuild = false;
+  let config: ResolvedConfig;
+  let timer: NodeJS.Timeout | null = null;
 
   function createTypes() {
-    if (!typeFile) return;
+    const data_json = "//" + JSON.stringify(data);
+    const types = Object.entries(data)
+      .map(([id, real]) => {
+        if (real.endsWith(".ts")) real = real.slice(0, real.length - 3);
+        if (real[0] == "/" || real[0] == "\\") real = "." + real;
+        if (real.startsWith(config.root))
+          real = "." + real.slice(config.root.length);
 
-    const dTS =
-      `//${JSON.stringify(data)}\n` +
-      Object.entries(data)
-        .map(([id, realPath]) => {
-          return `
-            declare module "${id}" {
-              const mod:import("comlink").Remote<typeof import("${
-                realPath.endsWith(".ts")
-                  ? realPath.slice(0, realPath.length - 3)
-                  : realPath
-              }")>
-              export default mod
-            }
-          `;
-        })
-        .join("");
+        return `
+        declare module "${id}" {
+          const mod: () => import("comlink").Remote<typeof import("${real}")>
+          export default mod
+        }
+      `;
+      })
+      .join("");
 
-    writeFileSync(typeFile, dTS);
+    writeFileSync(typesFile, data_json + types);
   }
-  let timer: NodeJS.Timeout | null;
-  let isBuild = false;
 
-  return {
+  return inWorker({
     name: "comlink",
     configResolved(resolvedConfig) {
-      if (types) {
-        typeFile = join(resolvedConfig.root, "comlink.d.ts");
-      }
       isBuild = resolvedConfig.command === "build";
-
-      if (types && existsSync(typeFile)) {
-        data = JSON.parse(
-          readFileSync(typeFile, "utf-8").split("\n")[0].slice(2)
-        );
+      config = resolvedConfig;
+      if (types) {
+        typesFile = join(config.root, typeFile);
+        if (!isBuild && existsSync(typesFile)) {
+          data = JSON.parse(
+            readFileSync(typesFile, "utf-8").split("\n")[0].slice(2)
+          );
+        }
       }
     },
     async resolveId(id, importer) {
-      if (id.includes(SCHEMA_WORKER)) {
+      if (id.startsWith(internal_schema)) {
         return id;
       }
-      if (id.startsWith(SCHEMA_MAIN_THREAD)) {
-        const realPath = id.slice(SCHEMA_MAIN_THREAD.length);
-        const newId = (await this.resolve(realPath, importer))?.id;
+      if (!id.startsWith(schema)) {
+        return;
+      }
 
-        if (!newId) throw Error(`Cannot find module '${realPath}'`);
+      const realPath = id.slice(schema.length);
+      const realID = (await this.resolve(realPath, importer))?.id.slice(
+        config.root.length
+      );
 
-        if (types && data[id] !== newId) {
-          data[id] = newId;
+      if (!realID) throw new Error(`Worker module ${realPath} not found`);
+
+      if (types) {
+        if (data[id] != realID) {
+          data[id] = realID;
 
           if (!timer) {
             timer = setTimeout(() => {
-              timer = null;
               createTypes();
+              timer = null;
             }, 2000);
           }
         }
-
-        return SCHEMA_MAIN_THREAD + newId;
       }
+
+      return schema + realID;
     },
-    closeBundle() {
-      if (types) createTypes();
+    buildEnd() {
+      if (types) {
+        createTypes();
+      }
     },
     async load(id) {
-      if (id.startsWith(SCHEMA_MAIN_THREAD)) {
-        const realPath = id.slice(SCHEMA_MAIN_THREAD.length);
+      if (id.startsWith(schema)) {
+        const baseId = `${internal_schema}${id.slice(schema.length)}`;
+        let url = `/@id/${baseId}`;
 
+        if (isBuild) {
+          const bundle = await rollup({
+            input: baseId,
+            plugins: config.plugins.filter(
+              (v) =>
+                (v as any)._inWorker || internal_worker_plugins.includes(v.name)
+            ),
+          });
+          let code: string;
+          try {
+            const { output } = await bundle.generate({
+              format: "iife",
+              sourcemap: config.build.sourcemap,
+            });
+
+            code = output[0].code;
+          } finally {
+            await bundle.close();
+          }
+
+          const content = Buffer.from(code);
+          const p = id.split(/\/|\\/g);
+          const b = p[p.length - 1].split(".");
+          const basename = b.slice(0, b.length - 1).join(".");
+          const contentHash = getAssetHash(content);
+          const fileName = join(
+            config.build.assetsDir,
+            `${basename}.${contentHash}.js`
+          );
+          url = `__VITE_ASSET__${this.emitFile({
+            fileName,
+            type: "asset",
+            source: code,
+          })}__`;
+        }
         return `
-          import { wrap } from "comlink";
-          import MyWorker from "${SCHEMA_WORKER}${realPath}?worker"
-          export default wrap(MyWorker())
+          import { wrap } from 'comlink'
+
+          export default () => wrap(new Worker('${url}'${isBuild ? ", {type: 'module'}" : ""}))
         `;
       }
-      if (id.startsWith(SCHEMA_WORKER) && id.endsWith("?worker")) {
-        return "";
-      }
-      if (id.includes(SCHEMA_WORKER) && id.endsWith("?worker_file")) {
-        const path = id.split(SCHEMA_WORKER)[1].split("?")[0];
 
+      if (id.startsWith(internal_schema)) {
         return `
-          import * as workerContent from "${path}"
-          import { expose } from "comlink";
-          expose(workerContent);
+          ${!isBuild ? "import '/@vite/env'" : ""}
+          import { expose } from 'comlink'
+          import * as m from '${id.slice(internal_schema.length)}'
+          
+          expose(m)
         `;
       }
     },
-  };
+  });
 }
