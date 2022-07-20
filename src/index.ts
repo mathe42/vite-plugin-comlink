c636b47b2362dc9b36dfcf13d821d5f01f564c71
mathe42 <2pi_r2@gmx.de>
2022-07-20 23:13:37 +0200

    make code simpler

--- src/index.ts
+++ src/index.ts
@@ -6,48 +6,46 @@ const importMetaUrl = `${"import"}.meta.url`;
 const urlPrefix_normal = "internal:comlink:";
 const urlPrefix_shared = "internal:comlink-shared:";
 
-let mode = "";
+const workerWrap = (url: string) => `
+import {expose} from 'comlink';
+import * as api from '${url}';
+expose(api);`
+
+const sharedWorkerWrap = (url: string) => `
+import {expose} from 'comlink';
+import * as api from '${url}';
+
+addEventListener('connect', (event) => {
+  const port = event.ports[0];
+
+  expose(api, port);
+  // We might need this later...
+  // port.start();
+})`
 
 export function comlink({
   replacement = "Worker",
   replacementShared = "SharedWorker",
 } = {}): Plugin {
+  let mode = "";
+
   return {
     name: "comlink",
     configResolved(conf) {
       mode = conf.mode;
     },
     resolveId(id) {
-      if (id.includes(urlPrefix_normal) || id.includes(urlPrefix_shared))
+      if (id.includes(urlPrefix_normal) || id.includes(urlPrefix_shared)) {
         return id;
+      }
     },
     async load(id) {
       if (id.includes(urlPrefix_normal)) {
-        const realID = normalizePath(id.replace(urlPrefix_normal, ""));
-
-        return `
-          import {expose} from 'comlink'
-          import * as api from '${normalizePath(realID)}'
-
-          expose(api)
-        `;
+        return workerWrap(normalizePath(id.replace(urlPrefix_normal, "")))
       }
 
       if (id.includes(urlPrefix_shared)) {
-        const realID = normalizePath(id.replace(urlPrefix_normal, ""));
-
-        return `
-          import {expose} from 'comlink'
-          import * as api from '${normalizePath(realID)}'
-
-          addEventListener('connect', (event) => {
-            const port = event.ports[0]; 
-              
-            expose(api, port);
-            // We might need this later...
-            // port.start()
-          })
-        `;
+        return sharedWorkerWrap(normalizePath(id.replace(urlPrefix_shared, "")))
       }
     },
     transform(code: string, id: string) {
@@ -55,13 +53,14 @@ export function comlink({
       if (
         !code.includes("ComlinkWorker") &&
         !code.includes("ComlinkSharedWorker")
-      )
+      ) {
         return;
+      }
 
       const workerSearcher =
         /\bnew\s+(ComlinkWorker|ComlinkSharedWorker)\s*\(\s*new\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*\)\s*(.*)\)/g;
 
-      let mStr: MagicString = new MagicString(code);
+      const mStr: MagicString = new MagicString(code);
 
       function workerReplacer(
         match: string,
@@ -71,35 +70,22 @@ export function comlink({
       ) {
         url = url.slice(1, url.length - 1);
 
-        const index = code.indexOf(match);
-
+        const startOfThisCode = code.indexOf(match);
         const restKommaIndex = rest.indexOf(",");
 
-        let replacementClass =
-          type === "ComlinkWorker" ? replacement : replacementShared;
+        const options: WorkerOptions & { replacement?: string } = restKommaIndex === -1 ? {} : JSON5.parse(
+          rest
+            .slice(restKommaIndex + 1)
+            .split(")")[0]
+            .trim()
+        );
 
-        if (restKommaIndex !== -1) {
-          const opt = JSON5.parse(
-            rest
-              .slice(restKommaIndex + 1)
-              .split(")")[0]
-              .trim()
-          );
-
-          if (mode === "development") {
-            opt.type = "module";
-          }
-
-          if (opt.replacement) {
-            replacementClass = opt.replacement;
-          }
-
-          rest = `,${JSON.stringify(opt)}`
-        } else {
-          if (mode === "development") {
-            rest += ',{type: "module"}';
-          }
+        if (mode === "development") {
+          options.type = "module";
         }
+
+        const replacementClass =
+          options.replacement ? options.replacement : (type === "ComlinkWorker" ? replacement : replacementShared)
 
         const prefix = type === "ComlinkWorker" ? urlPrefix_normal : urlPrefix_shared
 
@@ -108,13 +94,13 @@ export function comlink({
             new URL(
               '${prefix}${url}', 
               ${importMetaUrl}
-            )
-            ${rest}
+            ),
+            ${JSON.stringify(options)}
           )
           ${type === "ComlinkSharedWorker" ? ".port" : ""}
         )`;
 
-        mStr.overwrite(index, index + match.length, insertCode);
+        mStr.overwrite(startOfThisCode, startOfThisCode + match.length, insertCode);
         return match;
       }
 
